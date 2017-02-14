8f159d9e250fa4b4ac80e9f16e9ea1f5d58d21a9
hjy628 <hjy628@gmail.com>
2017-02-14 13:49:02 +0800

    本地测试学习积攒下来的小家当

---
+++ concurrency/src/other/ProducerConsumerExample.java
@@ -0,0 +1,12 @@
+package other;
+
+/**
+ * Created by hjy on 17-2-14.
+ */
+public class ProducerConsumerExample {
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+}
