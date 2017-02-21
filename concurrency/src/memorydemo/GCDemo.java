f3f9e09a1c2910d7a990e94f5eab8671705266da
hjy628 <hjy628@gmail.com>
2017-02-21 09:43:35 +0800

    JVM新生代大小调整策略demo

--- concurrency/src/memorydemo/GCDemo.java
+++ concurrency/src/memorydemo/GCDemo.java
@@ -9,6 +9,11 @@ import java.util.List;
  * 新生代设置过小时，1,minor GC次数更加频繁 2,有可能导致minor GC对象直接进入旧生代，如此占据旧生代剩余空间，触发Full GC
  * JVM参数： -Xms135M -Xmx135M -Xmn20M -XX:+UseSerialGC  执行了7次minor GC 2次Full GC
  * JVM参数： -Xms135M -Xmx135M -Xmn30M -XX:+UseSerialGC  执行了4次minor GC 1次Full GC
+ * 避免新生代大小设置过大
+ * 新生代设置过大时，1,旧生代变小，Full GC次数更加频繁 2, minor GC 耗时大幅度增加
+ * JVM参数： -Xms135M -Xmx135M -Xmn105M -XX:+UseSerialGC  执行了4次minor GC 1次Full GC
+ *
+ * 大多数场景下，新生代占堆内存33%左右比较合适
  */
 public class GCDemo {
 
