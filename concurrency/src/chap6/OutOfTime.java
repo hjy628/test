4e381c8700d35696045ac34c8c4164baf5b1d3af
hjy628 <hjy628@gmail.com>
2017-03-06 17:49:19 +0800

    Timer的缺陷

---
+++ concurrency/src/chap6/OutOfTime.java
@@ -0,0 +1,26 @@
+package chap6;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by hjy on 17-3-6.
+ */
+public class OutOfTime {
+
+    public static void main(String[] args) throws Exception{
+        Timer timer = new Timer();
+        timer.schedule(new ThrowTask(),1);
+        TimeUnit.SECONDS.sleep(1);
+        timer.schedule(new ThrowTask(),1);
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    static class ThrowTask extends TimerTask{
+        @Override
+        public void run() {
+            throw new RuntimeException();
+        }
+    }
+}
